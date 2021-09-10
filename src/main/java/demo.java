61c3224d8b56ab98edb7c051705e38b41e700b00
KeshavKumar <srinivasa1011@gmail.com>
2021-09-10 19:06:20 +0530

    reviewd

--- src/main/java/demo.java
+++ src/main/java/demo.java
@@ -7,7 +7,7 @@ public class demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("demo....");
+		System.out.println("demo..file..");
 
 	}
 
